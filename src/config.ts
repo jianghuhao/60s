3c363ed94be876e8bdab80c2ca93e170ab8a6f0e
jianghuhao <admin@jianghu.blog>
2025-06-25 19:22:30 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@ export const config = {
   host: process.env.HOST || '0.0.0.0',
   port: process.env.PORT ? +process.env.PORT : 4399,
   group: '595941841',
-  author: 'Viki <hi@viki.moe>',
-  github: 'https://github.com/vikiboss/60s',
+  author: 'gkzhan <gkzhan@gmail.com>',
+  github: 'https://github.com/jianghuhao/60s',
   encodingParamName: process.env.ENCODING_PARAM_NAME || 'encoding',
 }
 
